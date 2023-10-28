41baedd3715f1c3fba19ce5662509603453854be
joelson_luiz <joelsonluiz2010@gmail.com>
2023-10-28 19:20:02 -0300

    calculador de expectativa de vida criado

---
+++ Pessoa.cs
@@ -0,0 +1,23 @@
+namespace Objetos
+{
+
+    public class Pessoa
+    {
+
+        public string? nome;
+        public DateTime nascimento;
+
+        public Pessoa()
+        {
+
+        }
+
+        public DateTime adicionaCemAnos(DateTime nascimento)
+        {
+            DateTime resultado = nascimento.AddYears(100);
+            return resultado;
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Objetos;
+
+Pessoa pessoa = new Pessoa();
+
+try
+{
+    Console.WriteLine("Escreva o seu nome");
+    pessoa.nome = Console.ReadLine();
+
+    Console.WriteLine("Escreva a data de seu nascimento, exemplo: dia/mês/ano e aperte ENTER");
+    string? data = Console.ReadLine();
+
+    pessoa.nascimento = DateTime.Parse(data);
+
+    Console.WriteLine("Nome: " + pessoa.nome);
+    Console.WriteLine("Data de Nascimento: " + pessoa.nascimento.ToString("dd/MM/yyyy"));
+    Console.WriteLine("Viverá até: " + pessoa.adicionaCemAnos(pessoa.nascimento).ToString("dd/MM/yyyy"));
+}
+catch (System.FormatException)
+{
+    Console.WriteLine("Data escrita no formato inválido, tente novamente.");
+}
